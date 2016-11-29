08a95f33260cdb02db9b4963a3a808d55260beab
han <yhappy_2008@126.com>
2016-11-30 00:24:25 +0800

    API: errorhandler

--- app/api_1_0/errors.py
+++ app/api_1_0/errors.py
@@ -4,3 +4,7 @@ def forbidden(message):
     response = jsonify({'error': 'forbidden', 'message': message})
     response.status_code = 403
     return response
+
+@api.errorhandler(ValidationError)
+def validation_error(e):
+    return bad_request(e.args[0])

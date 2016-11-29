4b329bbd3394b4ecbb117cc7702025f86963a3d5
han <yhappy_2008@126.com>
2016-11-30 00:16:04 +0800

    POST: get body from json

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from . import db, login_manager
 import hashlib
 from markdown import markdown
 import bleach
+from app.exceptions import ValidationError
 
 
 class Role(db.Model):
@@ -92,6 +93,13 @@ class Post(db.Model):
             'comment_count': self.comments.count()
         }
         return json_post
+
+    @staticmethod
+    def from_json(json_post):
+        body = json_post.get('body')
+        if body is None or body == '':
+            raise ValidationError('post does not have a body')
+        return Post(body=body)
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
